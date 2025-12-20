542c75a3dbcf2e55258e81c98ee1ec5ba1e5ffed
Joseph Pickering <josephpickering9@gmail.com>
2025-12-20 23:52:13 +0000

    lint

--- app/components/filters/list/VenueFilterList.vue
+++ app/components/filters/list/VenueFilterList.vue
@@ -48,7 +48,6 @@ const filteredVenues = computed(() => {
 });
 
 function selectVenue(venueId: string) {
-  console.log('VenueFilterList: selectVenue called with', venueId);
   emit('select', venueId);
 }
 
