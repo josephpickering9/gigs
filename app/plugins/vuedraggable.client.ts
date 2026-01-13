6985191616c026531bd3e83da24a6dae39734467
Joseph Pickering <josephpickering9@gmail.com>
2026-01-13 19:24:20 +0000

    api

--- app/plugins/vuedraggable.client.ts
+++ app/plugins/vuedraggable.client.ts
@@ -1,5 +1,5 @@
-import draggable from 'vuedraggable'
+import draggable from 'vuedraggable/src/vuedraggable'
 
 export default defineNuxtPlugin((nuxtApp) => {
-    nuxtApp.vueApp.component('draggable', draggable)
+    nuxtApp.vueApp.component('Draggable', draggable)
 })
